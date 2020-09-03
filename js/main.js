4fac0c589d0b0d4557d144a58f95684529fbfd0c
damiloyola <damiloyola@gmail.com>
2020-09-03 00:18:01 -0300

    agrego id a cada elemento

--- js/main.js
+++ js/main.js
@@ -1,52 +1,56 @@
 //Clase Productos
 class Productos {
-  constructor(nombre, valor){
-      this.nombre = nombre;
-      this.valor = valor;
+  constructor(nombre, valor) {
+    this.nombre = nombre;
+    this.valor = valor;
   }
 }
 //Objetos - Array productos
-let Products = [
-  { 
-     nombre: "Beni Shoga",
-      valor: 10,
+let producto = [
+  {
+    nombre: "Beni Shoga",
+    valor: 10,
+    id: 1,
   },
   {
-      nombre: "Do Chua",
-      valor: 10,
+    nombre: "Do Chua",
+    valor: 10,
+    id: 2,
   },
   {
-      nombre: "Limones",
-      valor: 10,
+    nombre: "Limones",
+    valor: 10,
+    id: 3,
   },
   {
-      nombre: "Sauerkraut",
-      valor: 10,
+    nombre: "Sauerkraut",
+    valor: 10,
+    id: 4,
   },
   {
-      nombre: "Alcaparras",
-      valor: 10,
+    nombre: "Alcaparras",
+    valor: 10,
+    id: 5,
   },
   {
-      nombre: "Remolachas",
-      valor: 10,
-  }
-
-])
+    nombre: "Remolachas",
+    valor: 10,
+    id: 6,
+  },
+];
 
 let carro = [];
 localStorage.setItem("carro", JSON.stringify(carro));
 
-
 //añadir al carro -D-
-const btnAñadir = document.querySelectorAll("button");
+const btnAñadir = document.querySelectorAll(".btn_añadir");
 const añadirHandler = (e) => {
   console.log(e.target.attributes.id.value);
   //tomar id del producto seleccionado
   const id = e.target.attributes.id.value;
   const carro = JSON.parse(localStorage.getItem("carro"));
   //buscar en array principal de productos
-  productos.forEach((i) => {
+  producto.forEach((i) => {
     if (i.id === id) {
       carro.push(i);
     }
@@ -56,38 +60,31 @@ const añadirHandler = (e) => {
 };
 btnAñadir.forEach((i) => i.addEventListener("click", añadirHandler));
 
-
 // TODO ------------------------------ AGOS
 
 // Selección del metodo de delivery de la compra
 
 // Primero: guardar el formulario.
 
-const deliveryMethodForm = document.querySelector('#selectionDelivery');
+const deliveryMethodForm = document.querySelector("#selectionDelivery");
 
 // Segundo: guardar la section de envio y retiro
 
-const deliveryMethod = document.querySelector('#deliveryOption');
+const deliveryMethod = document.querySelector("#deliveryOption");
 
-const pickUpMethod = document.querySelector('#pickUpOpcion');
+const pickUpMethod = document.querySelector("#pickUpOpcion");
 
 // Tercero: realizar el evento sobre el form.
-const showDelivery = deliveryMethodForm.addEventListener('click', function () {
+const showDelivery = deliveryMethodForm.addEventListener("click", function () {
   // Cuarto: guardar el valor clickeado por el usuario
   let deliveryMethodChosen = deliveryMethodForm.methodChosen.value;
 
   // Quinto: hacer las comparaciones para que se escondan o se agreguen las clases.
   if (deliveryMethodChosen === "Entrega a domicilio") {
-    deliveryMethod.classList.remove('hidden');
-    pickUpMethod.classList.add('hidden');
+    deliveryMethod.classList.remove("hidden");
+    pickUpMethod.classList.add("hidden");
   } else {
-    pickUpMethod.classList.remove('hidden');
-    deliveryMethod.classList.add('hidden');
+    pickUpMethod.classList.remove("hidden");
+    deliveryMethod.classList.add("hidden");
   }
-
 });
-
-
-
-
-
