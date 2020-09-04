e9d383c0da2be9d516a12338c2ef905d924db19f
Jesus <jesusignaciolencina@gmail.com>
2020-09-03 21:38:10 -0300

    Commit para guardar lo poco que hice del recibo antes de ponerme a romper todo con el fondo de pepinos

---
+++ js/receipt.js
@@ -0,0 +1,16 @@
+//ARRAY DE ITEMS
+var arrayItems = JSON.parse(localStorage.getItem("carro"));
+var precios = 0;
+//OBJETOS
+var lista = document.querySelector(".rec_list");
+
+agregarItems();
+//FUNCIONES
+function agregarItems() {
+    for (let index = 0; index < arrayItems.length; index++) {
+        var agregado = lista.createChild("LI");
+        agregado.innerHTML= arrayItems[index].nombre;
+        lista.appendChild(agregado);
+    }
+}
+
