4de22dfc2d377896a8afe97b9207dc709aab5a0f
Mafer <jmmariafernanda15@gmail.com>
2020-09-08 22:01:59 -0300

    cualquier guilada

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ let products = [
     nombre: "Beni Shoga",
     valor: 10,
     id: 1,
-    img: "assets\store_files\image-asset.jpeg" 
   },
   {
     nombre: "Do Chua",
@@ -66,10 +65,3 @@ const añadirHandler = (e) => {
   console.log(localStorage.getItem("carro"));
 };
 btnAñadir.forEach((i) => i.addEventListener("click", añadirHandler));
-
-// TODO: Mostrar en el carrito los objetos seleccionados -Mafer-
-/* Validar si hay productos cargados 
-Traer productos del localStorage
-Si hay productos: recorrer Array*, renderizar/crear elementos con cada objeto 
-*Cuando se recorra el Array para los productos hacer que tome los precios (value)
-Crear función para que los sume y de el total */
